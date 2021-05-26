8635a1eef4b7f8e66bbac5dd7013c7a59c008d51
Agnieszka Kurzajewska <agnieszka.kurzajewska@cloudservices.pl>
2021-05-27 00:57:59 +0200

    first page

---
+++ symfony/src/Controller/FirstController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class FirstController
+{
+
+    public function homepage(): Response
+    {
+        return new Response(
+            '<html><body><h1>Welcome!</h1></body></html>'
+        );
+    }
+}
